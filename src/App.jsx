7afd6ab25bfe098a24b69a1f60f4768cffd1a932
fersimedyoussef <medyoussef.fersi@looyas.com>
2024-05-11 23:56:30 +0100

    Update name in siteProps object

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Mohaamed Youssef Fersi",
+  name: "Mohamed Youssef Fersi",
   title: "Microsoft Learn Student Ambassador",
   email: "fersimohamedyoussef@gmail.com",
   gitHub: "Fersi60",
